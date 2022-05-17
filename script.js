748654efd43098290f17c11a1d659e05e13920c9
bengigenc <genc.bengi@gmail.com>
2022-05-17 17:43:41 +0300

    Fix linter errors

--- script.js
+++ script.js
@@ -28,49 +28,49 @@ const speakersData = [
     speakerImg: 'images/speakers/bamidele-cousineau.jpeg',
     speakerName: 'Bamidele Cousinea',
     speakerTitle: 'Head chef of Of Gene Acıktım',
-    speakerInfo: 'Bamidele started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
+    speakerInfo: 'Bamidele started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.',
   },
   // speaker2
   {
     speakerImg: 'images/speakers/danny-tilki.jpeg',
     speakerName: 'Danny Tilki',
     speakerTitle: 'Head chef of Of Yiyin Gari',
-    speakerInfo: 'Danny started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
+    speakerInfo: 'Danny started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.',
   },
   // speaker3
   {
     speakerImg: 'images/speakers/duru-teke.jpeg',
     speakerName: 'Duru Teke',
     speakerTitle: 'Head chef of Of Gene Acıktım',
-    speakerInfo: 'Duru started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
+    speakerInfo: 'Duru started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.',
   },
-    // speaker4
-    {
+  // speaker4
+  {
     speakerImg: 'images/speakers/hla-forney.jpeg',
     speakerName: 'Hla Forney',
     speakerTitle: 'Head chef of Of Gene Acıktım',
-    speakerInfo: 'Hla started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
-    },
-    // speaker5
-    {
+    speakerInfo: 'Hla started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.',
+  },
+  // speaker5
+  {
     speakerImg: 'images/speakers/jyothi-eerkens.jpeg',
     speakerName: 'Jhothi Eerkens',
     speakerTitle: 'Head chef of Of Gene Acıktım',
-    speakerInfo: 'Jhothi started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
-    },
-    // speaker6
-    {
+    speakerInfo: 'Jhothi started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.',
+  },
+  // speaker6
+  {
     speakerImg: 'images/speakers/paget-basso.jpeg',
     speakerName: 'Paget Basso',
     speakerTitle: 'Head chef of Of Gene Acıktım',
-    speakerInfo: 'Paget started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
-    },
+    speakerInfo: 'Paget started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.',
+  },
 ];
 
 const speakers = document.querySelector('.speakers-container');
 
 Array.from(speakers.children).forEach((child, index) => {
-    child.innerHTML = `
+  child.innerHTML = `
     <div class="speaker">
       <div class="photo-container">
         <img class="photo" src="${speakersData[index].speakerImg}">
@@ -82,12 +82,11 @@ Array.from(speakers.children).forEach((child, index) => {
         <p class="speaker-about">${speakersData[index].speakerInfo}</p>
       </div>
     </div>
-    `
-})
-
+    `;
+});
 
 {
-    /* <div class="speaker">
+  /* <div class="speaker">
       <div class="photo-container">
         <img class="photo">
       </div>
@@ -100,8 +99,4 @@ Array.from(speakers.children).forEach((child, index) => {
     </div> */
 }
 
-
-
-
-
-console.log('Ok, you can sleep now')
+console.log('Ok, you can sleep now');
