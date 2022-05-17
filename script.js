03da0430fe5ff8149abd4b833f6127ceda03b3c0
bengigenc <genc.bengi@gmail.com>
2022-05-17 16:23:49 +0300

    function mobile menu button and style mobile menu

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log('Hello Bengi, please don\'t sleep yet');
+
+const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.mobile-nav');
+const mobileLinks = document.querySelector('.mobile-links');
+const body = document.querySelector('body');
+const burgerField = document.querySelector('.burger-nav');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+    body.classList.toggle('fixed');
+    burgerField.classList.toggle('active');
+});
+
+// mobileLinks.forEach((link) => link.addEventListener('click', () => {
+//     burger.classList.toggle('active');
+//     mobileMenu.classList.toggle('active');
+//     body.classList.toggle('fixed');
+// }));
+
+
+
+
+
+console.log('Ok, you can sleep now')
