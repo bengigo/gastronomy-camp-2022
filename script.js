3bcad4438bd01b6aa4dc5622e2d55471f8067513
bengigenc <genc.bengi@gmail.com>
2022-05-17 17:31:56 +0300

    Display speakers dynamically and set speaker photos size

--- script.js
+++ script.js
@@ -22,50 +22,68 @@ mobileLinks.forEach((link) => link.addEventListener('click', () => {
 }));
 
 //array to hold speakers data
-const speakers = [
+const speakersData = [
     //speaker1
     {
         speakerImg: 'images/speakers/bamidele-cousineau.jpeg',
         speakerName: 'Bamidele Cousinea',
         speakerTitle: 'Head chef of Of Gene Acıktım',
         speakerInfo: 'Bamidele started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
-    }
+    },
     //speaker2
     {
         speakerImg: 'images/speakers/danny-tilki.jpeg',
         speakerName: 'Danny Tilki',
         speakerTitle: 'Head chef of Of Yiyin Gari',
         speakerInfo: 'Danny started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
-    }
+    },
     //speaker3
     {
         speakerImg: 'images/speakers/duru-teke.jpeg',
         speakerName: 'Duru Teke',
         speakerTitle: 'Head chef of Of Gene Acıktım',
         speakerInfo: 'Duru started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
-    }
-    /speaker4
+    },
+    //speaker4
     {
         speakerImg: 'images/speakers/hla-forney.jpeg',
         speakerName: 'Hla Forney',
         speakerTitle: 'Head chef of Of Gene Acıktım',
         speakerInfo: 'Hla started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
-    }
+    },
     //speaker5
     {
         speakerImg: 'images/speakers/jyothi-eerkens.jpeg',
         speakerName: 'Jhothi Eerkens',
         speakerTitle: 'Head chef of Of Gene Acıktım',
         speakerInfo: 'Jhothi started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
-    }
+    },
     //speaker6
     {
         speakerImg: 'images/speakers/paget-basso.jpeg',
         speakerName: 'Paget Basso',
         speakerTitle: 'Head chef of Of Gene Acıktım',
         speakerInfo: 'Paget started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
-    }
-]
+    },
+];
+
+const speakers = document.querySelector('.speakers-container');
+
+Array.from(speakers.children).forEach((child, index) => {
+    child.innerHTML = `
+    <div class="speaker">
+      <div class="photo-container">
+        <img class="photo" src="${speakersData[index].speakerImg}">
+      </div>
+      <div class="speaker-info">
+        <p class="name">${speakersData[index].speakerName}</p>
+        <p class="title">${speakersData[index].speakerTitle}</p>
+        <hr>
+        <p class="speaker-about">${speakersData[index].speakerInfo}</p>
+      </div>
+    </div>
+    `
+})
 
 
 {/* <div class="speaker">
