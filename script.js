e0e7a3ad8a24f9f5cb6016aa54e74e1228093869
bengigenc <genc.bengi@gmail.com>
2022-05-17 17:39:24 +0300

    Fix linter error

--- script.js
+++ script.js
@@ -25,45 +25,45 @@ mobileLinks.forEach((link) => link.addEventListener('click', () => {
 const speakersData = [
   // speaker1
   {
-      speakerImg: 'images/speakers/bamidele-cousineau.jpeg',
-      speakerName: 'Bamidele Cousinea',
-      speakerTitle: 'Head chef of Of Gene Acıktım',
-      speakerInfo: 'Bamidele started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
+    speakerImg: 'images/speakers/bamidele-cousineau.jpeg',
+    speakerName: 'Bamidele Cousinea',
+    speakerTitle: 'Head chef of Of Gene Acıktım',
+    speakerInfo: 'Bamidele started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
   },
   // speaker2
   {
-      speakerImg: 'images/speakers/danny-tilki.jpeg',
-      speakerName: 'Danny Tilki',
-      speakerTitle: 'Head chef of Of Yiyin Gari',
-      speakerInfo: 'Danny started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
+    speakerImg: 'images/speakers/danny-tilki.jpeg',
+    speakerName: 'Danny Tilki',
+    speakerTitle: 'Head chef of Of Yiyin Gari',
+    speakerInfo: 'Danny started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
   },
   // speaker3
   {
-      speakerImg: 'images/speakers/duru-teke.jpeg',
-      speakerName: 'Duru Teke',
-      speakerTitle: 'Head chef of Of Gene Acıktım',
-      speakerInfo: 'Duru started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
+    speakerImg: 'images/speakers/duru-teke.jpeg',
+    speakerName: 'Duru Teke',
+    speakerTitle: 'Head chef of Of Gene Acıktım',
+    speakerInfo: 'Duru started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
   },
     // speaker4
     {
-      speakerImg: 'images/speakers/hla-forney.jpeg',
-      speakerName: 'Hla Forney',
-     speakerTitle: 'Head chef of Of Gene Acıktım',
-      speakerInfo: 'Hla started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
+    speakerImg: 'images/speakers/hla-forney.jpeg',
+    speakerName: 'Hla Forney',
+    speakerTitle: 'Head chef of Of Gene Acıktım',
+    speakerInfo: 'Hla started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
     },
     // speaker5
     {
-     speakerImg: 'images/speakers/jyothi-eerkens.jpeg',
-      speakerName: 'Jhothi Eerkens',
-      speakerTitle: 'Head chef of Of Gene Acıktım',
-      speakerInfo: 'Jhothi started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
+    speakerImg: 'images/speakers/jyothi-eerkens.jpeg',
+    speakerName: 'Jhothi Eerkens',
+    speakerTitle: 'Head chef of Of Gene Acıktım',
+    speakerInfo: 'Jhothi started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
     },
     // speaker6
     {
-        speakerImg: 'images/speakers/paget-basso.jpeg',
-        speakerName: 'Paget Basso',
-        speakerTitle: 'Head chef of Of Gene Acıktım',
-        speakerInfo: 'Paget started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
+    speakerImg: 'images/speakers/paget-basso.jpeg',
+    speakerName: 'Paget Basso',
+    speakerTitle: 'Head chef of Of Gene Acıktım',
+    speakerInfo: 'Paget started learning in their grandparents kitchen and became the head chef of Of Gene Acıktım at the age of 20. They also have a cooking school for children.'
     },
 ];
 
