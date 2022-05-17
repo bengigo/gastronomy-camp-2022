6bc30c5ce620a2ffc44d0d92d89d5195384694a8
bengigenc <genc.bengi@gmail.com>
2022-05-17 16:39:09 +0300

    enable links of the mobile navigation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('Hello Bengi, please don\'t sleep yet');
 
 const burger = document.querySelector('.burger');
 const mobileMenu = document.querySelector('.mobile-nav');
-const mobileLinks = document.querySelector('.mobile-links');
+const mobileLinks = document.querySelectorAll('.mobile-links');
 const body = document.querySelector('body');
 const burgerField = document.querySelector('.burger-nav');
 
@@ -13,11 +13,12 @@ burger.addEventListener('click', () => {
     burgerField.classList.toggle('active');
 });
 
-// mobileLinks.forEach((link) => link.addEventListener('click', () => {
-//     burger.classList.toggle('active');
-//     mobileMenu.classList.toggle('active');
-//     body.classList.toggle('fixed');
-// }));
+mobileLinks.forEach((link) => link.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+    body.classList.toggle('fixed');
+    burgerField.classList.toggle('active');
+}));
 
 
 
